19c1cba2b35459ce7700f86b5cff31525f276801
xiedeshou <499452441@qq.com>
2018-09-21 09:56:26 +0800

    (+)Eureka-高可用

---
+++ spring-cloud-eureka-server-ha/src/main/java/cn/lqdev/learning/springcloud/eureka/EureakServiceHaApplication.java
@@ -0,0 +1,41 @@
+package cn.lqdev.learning.springcloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Eureka服务端
+ * @author oKong
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+@Slf4j
+public class EureakServiceHaApplication {
+	
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(EureakServiceHaApplication.class, args);
+		log.info("spring-cloud-eureka-service-ha启动!");
+	}
+	
+	/**
+	 * 忽略此路径下的CSRF令牌
+	 * @author oKong
+	 *
+	 */
+	@EnableWebSecurity
+	static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().ignoringAntMatchers("/eureka/**");
+			super.configure(http);
+		}
+	}	
+}
