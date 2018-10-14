c83c0058d673119caa3cf811df12ab6d31fdd0f8
xds <499452441@qq.com>
2018-10-14 23:47:53 +0800

    (*)第六章：Hystrix监控面板及数据聚合(Turbine)

--- spring-cloud-hystrix/src/main/java/cn/lqdev/learning/springcloud/hystrix/HystrixApplication.java
+++ spring-cloud-hystrix/src/main/java/cn/lqdev/learning/springcloud/hystrix/HystrixApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
